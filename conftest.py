c4209acddf76d64d18aafe786f950580c8d300ee
Ashmita@04 <ashmitamajumder9@gmail.com>
2026-02-20 13:30:35 +0530

    Added screenshot on failure in conftest

--- conftest.py
+++ conftest.py
@@ -1,24 +1,24 @@
 import pytest
 from selenium import webdriver
-
-from utils.driver_factory import get_driver
 from utils.screenshot_utils import take_screenshot
-
 @pytest.fixture
 def driver(request):
-    driver=webdriver.Chrome()
+    driver = webdriver.Chrome()
     driver.get("https://www.ixigo.com/")
     driver.maximize_window()
     driver.implicitly_wait(5)
     yield driver
 
-    # if request.node.rep_call.failed:
-    #     take_screenshot(driver, request.node.name)
-    #
-    # driver.quit()
+    driver.quit()
 
 @pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item):
+def pytest_runtest_makereport(item, call):
     outcome = yield
     rep = outcome.get_result()
+
     setattr(item, "rep_" + rep.when, rep)
+
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            take_screenshot(driver, item.name)
